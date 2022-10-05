10475bf8ce4e5a149ae32340584e14908445217f
Nicolás Rodrigo Ávila Biskupovic <36216299+NicolasAvilaB@users.noreply.github.com>
2022-10-05 08:28:45 -0300

    Delete inventario-jardin-manque-backend-apirest directory

--- inventario-jardin-manque-backend-apirest/src/main/java/com/inventario/jardin/manquemavida/backend/apirest/models/dao/UsuarioDao.java
+++
@@ -1,9 +0,0 @@
-package com.inventario.jardin.manquemavida.backend.apirest.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.inventario.jardin.manquemavida.backend.apirest.models.entity.Usuario;
-
-public interface UsuarioDao extends CrudRepository<Usuario, Long> {
-
-}
